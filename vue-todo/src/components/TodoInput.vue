2461f3c05756c2e6e293aa64564a99ed44a81f33
kyomin <kim031504@naver.com>
2021-11-16 22:47:14 +0900

    인풋 컴포넌트 생성 및 등록

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" />
+    <button @click="addTodo" type="button">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    addTodo() {
+      console.log("addTodo");
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
