586d0d85222b80a8dc75cf15e5ac3febb6a3917f
kyomin <kim031504@naver.com>
2021-12-01 21:48:44 +0900

    추가 기능 구현 : 인풋 컴포넌트 props 속성 유효성 검사 및 타입 정의

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
+    <!--
+      input 컴포넌트를 따로 만들게 되면, 재활용성이 높아지면서
+      사용되어지는 여러 곳에 같은 스타일과 동작을 보장할 수 있다.
+    -->
     <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem" />
   </div>
 </template>
@@ -17,7 +21,7 @@ export default Vue.extend({
     };
   },
   methods: {
-    updateTodoText(value: any) {
+    updateTodoText(value: string) {
       this.todoText = value;
     },
     addTodoItem() {
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -10,10 +10,21 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-  props: ['item'],
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
   methods: {
-    handleInput(e: any) {
-      this.$emit('input', e.target.value);
+    handleInput(e: InputEvent) {
+      /* 
+        타입 어설션!
+        as 뒤의 타입이라고 개발자가 확신하며 VSCode에 이 타입임을 알린다.
+        개발자가 어셜션하는 타입을 실수할 수도 있기 때문에 완전히 안전한 방식은 아니다.
+      */
+      const eventTarget = e.target as HTMLInputElement;
+      this.$emit('input', eventTarget.value);
     },
     addTodo() {
       this.$emit('add');
