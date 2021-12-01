3a77713b058412e8cbbec13c25914a8111e52bc9
kyomin <kim031504@naver.com>
2021-12-01 22:30:45 +0900

    조회 기능 구현 : 할 일 추가 API 설계 및 구현

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,12 @@ import TodoListItem from './components/TodoListItem.vue';
 
 const STORAGE_KEY = 'vue-todo-ts-v1';
 const storage = {
+  save(todoItems: any[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
   fetch() {
-    const todoItems = localStorage.getItem(STORAGE_KEY) || [];
+    const todoItems = localStorage.getItem(STORAGE_KEY) || '[]';
     const result = JSON.parse(todoItems);
     return result;
   },
@@ -33,6 +37,7 @@ export default Vue.extend({
   data() {
     return {
       todoText: '',
+      todoItems: [] as any[],
     };
   },
   methods: {
@@ -41,12 +46,19 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = '';
     },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
